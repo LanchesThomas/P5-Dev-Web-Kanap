d4d29ee8d713c304a21911e778d289090c61a23d
Sotham <lanches.thomas@gmail.com>
2022-11-08 14:20:20 +0100

    add all products

--- front/js/script.js
+++ front/js/script.js
@@ -5,34 +5,32 @@ fetch('http://localhost:3000/api/products')
         }
     })
     .then((res) => {
-        kanap1();
-        
-        console.table(res)
-        for (index in res) {
+        kanap();
+        for (let index in res) {
             createElement();
         }
-
     })
     .catch((err) => {
         console.log('une Erreur est survenue');
     });
 
-    // fonction création des éléments
-    function createElement() {
-        lien = document.createElement('a');
-        document.getElementById('items').appendChild(lien);
-        article = document.createElement('article');
-        lien.appendChild(article);
-        img = document.createElement('img');
-        article.appendChild(img)
-        title = document.createElement('h3');
-        article.appendChild(title);
-        description = document.createElement('p');
-        article.appendChild(description)
-    }
+// fonction création des éléments
+function createElement() {
+    lien = document.createElement('a');
+    document.getElementById('items').appendChild(lien);
+    article = document.createElement('article');
+    lien.appendChild(article);
+    img = document.createElement('img');
+    article.appendChild(img);
+    title = document.createElement('h3');
+    article.appendChild(title);
+    description = document.createElement('p');
+    article.appendChild(description);
+}
 
-    // fonction d'ajout du premier canapé
-function kanap1() {
+// fonction d'ajout des canapés
+
+function kanap() {
     fetch('http://localhost:3000/api/products')
         .then((res) => {
             if (res.ok) {
@@ -40,26 +38,19 @@ function kanap1() {
             }
         })
         .then((res) => {
-            document.querySelector('#items h3').innerHTML = res[0].name;
-            document.querySelector('#items img').src = res[0].imageUrl;
-            document.querySelector('#items p').innerHTML = res[0].description;
-            document.querySelector('#items a').href = './product.html?id=' + res[0]._id
-        });
-}
-
-function kanap2() {
-    fetch('http://localhost:3000/api/products')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
+            for (let i in res) {
+                // boucle ajout titre
+                let title = document.querySelectorAll('#items h3');
+                title[i].innerHTML = res[i].name;
+                // boucle ajout image
+                let img = document.querySelectorAll('#items img');
+                img[i].src = res[i].imageUrl;
+                // boucle ajout description
+                let description = document.querySelectorAll('#items p');
+                description[i].innerHTML = res[i].description;
+                // boucle ajout lien
+                let link = document.querySelectorAll('#items a');
+                link[i].href = './product.html?id=' + res[i]._id;
             }
-        })
-        .then((res) => {
-            console.table(res);
-            document.querySelector('#items h3').innerHTML = res[1].name;
-            document.querySelector('#items img').src = res[1].imageUrl;
-            document.querySelector('#items p').innerHTML = res[1].description;
-            document.querySelector('#items a').href = './product.html?id=' + res[1]._id
         });
 }
-
