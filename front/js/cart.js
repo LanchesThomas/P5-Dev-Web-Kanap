16c74edfb99327942e0314e28f2a6e6eb00f48f5
Sotham <lanches.thomas@gmail.com>
2022-11-09 10:36:39 +0100

    add évolution prix en focntion de la quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,31 +3,65 @@ let quantityProduct = localStorage.quantityProduct;
 let colorProduct = localStorage.colorProduct;
 
 fetch(`http://localhost:3000/api/products/${idProduct}`)
-.then((res) => {
-    if (res.ok) {
-        return res.json();
-    } })
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+    })
     .then((res) => {
         // ajout image
         let img = document.querySelector('.cart__item__img img');
         img.src = res.imageUrl;
         // ajout du titre
-        document.querySelector('.cart__item__content__description h2').innerText = res.name;
+        document.querySelector(
+            '.cart__item__content__description h2'
+        ).innerText = res.name;
         // ajout du prix
-        document.querySelector('.cart__item__content__description').lastElementChild.innerText = res.price * quantityProduct + ' €';
-        // ajout couleur
-        document.querySelector('.cart__item__content__description p').innerText = colorProduct;
-        // ajout quantité 
-        document.querySelector('.cart__item__content__settings__quantity input').value = quantityProduct;
+        document.querySelector(
+            '.cart__item__content__description'
+        ).lastElementChild.innerText = res.price * quantityProduct + ' €';
+        // ajout évolution du prix en fonction de la quantité
+        document
+            .querySelector('.cart__item__content__settings__quantity input')
+            .addEventListener('change', function () {
+                let newQuantity = document.querySelector(
+                    '.cart__item__content__settings__quantity input'
+                ).value;
 
-    })
-    
+                if (newQuantity > quantityProduct) {
+                    document.querySelector(
+                        '.cart__item__content__description'
+                    ).lastElementChild.innerText =
+                        res.price * newQuantity + ' €';
+                } else if (newQuantity < quantityProduct) {
+                    document.querySelector(
+                        '.cart__item__content__description'
+                    ).lastElementChild.innerText =
+                        res.price * newQuantity + ' €';
+                } else if (newQuantity == quantityProduct) {
+                  document.querySelector(
+                    '.cart__item__content__description'
+                ).lastElementChild.innerText = res.price * quantityProduct + ' €';
+                }
+            });
+
+        // ajout couleur
+        document.querySelector(
+            '.cart__item__content__description p'
+        ).innerText = colorProduct;
+        // ajout quantité
+        document.querySelector(
+            '.cart__item__content__settings__quantity input'
+        ).value = quantityProduct;
+    });
+
 createElement();
 
 function createElement() {
     let article = document.createElement('article');
     document.getElementById('cart__items').appendChild(article);
-    article.innerHTML = '<div class="cart__item__img">\
+    article.innerHTML =
+        '<div class="cart__item__img">\
     <img src="../images/product01.jpg" alt="Photographie d\'un canapé">\
   </div>\
   <div class="cart__item__content">\
@@ -39,11 +73,11 @@ function createElement() {
     <div class="cart__item__content__settings">\
       <div class="cart__item__content__settings__quantity">\
         <p>Qté : </p>\
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">\
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="">\
       </div>\
       <div class="cart__item__content__settings__delete">\
         <p class="deleteItem">Supprimer</p>\
       </div>\
     </div>\
-  </div>'
+  </div>';
 }
